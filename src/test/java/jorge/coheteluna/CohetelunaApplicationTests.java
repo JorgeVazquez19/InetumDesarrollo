3d844f2429b0e21d4c8052b74cf07b9fd18bdf1e
Jorge Vazquez Requejo <jorgitoovazquez@hotmail.com>
2024-06-05 11:10:30 +0200

    Add files via upload

---
+++ src/test/java/jorge/coheteluna/CohetelunaApplicationTests.java
@@ -0,0 +1,13 @@
+package jorge.coheteluna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohetelunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
